d6d5e3abd240b07e12e305bda8a2a43082a9e784
pymia <mia5419@gmail.com>
2018-06-08 16:53:45 +0800

    init model with given data

--- predict_ml.py
+++ predict_ml.py
@@ -1,11 +1,16 @@
-"""aaa"""
+"""predict batch size with defined Classifier"""
+"""version: machine learning"""
+
 from numpy import genfromtxt
 import numpy as np
 from sklearn.naive_bayes import BernoulliNB
+from sklearn.model_selection import train_test_split
+
+TRAIN_PATH = "train_cases.csv"
 
 
 def read_csv(path):
-    """aaa"""
+    """read csv file with genfromtxt to get numpy arr"""
     return genfromtxt(path, delimiter=',')
 
 
@@ -14,20 +19,24 @@ class Classifier(object):
 
     def __init__(self):
         clf = BernoulliNB()
-        train_x = np.random.randint(2, size=(6, 10))
-        train_y = np.array([1, 2, 3, 4, 4, 5])
+        train_dataset = read_csv(TRAIN_PATH)
+        dataset_x = [i[:6] for i in train_dataset]
+        dataset_y = [i[-1] for i in train_dataset]
+        train_x, test_x, train_y, test_y = train_test_split(
+            dataset_x, dataset_y, test_size=0.33, random_state=42)
         self.model = clf.fit(train_x, train_y)
+        self.test = test_x
 
-    def predict(self, test_arr):
+    def predict(self):
         """predict with input"""
+        test_arr = self.test
         return self.model.predict(test_arr)
 
 
 def main():
     """define model and predict"""
     model = Classifier()
-    test_arr = [[0, 0, 1, 1, 0, 0, 1, 1, 0, 0]]
-    predict_arr = model.predict(test_arr)
+    predict_arr = model.predict()
     print(predict_arr)
 
 
